b75a9fab9cd7dd4881e966292462cdc6021a2660
Dvir Azulay <dvir.azulay@gmail.com>
2015-01-04 16:36:11 +0000

    Fixed pathfinding dart package import; Added browser dart package, to convert from Dart to JS

--- client/dart/packages/pathfinding/test/test_util.dart
+++
@@ -1,7 +0,0 @@
-library pathfinding.test.util;
-
-import 'package:unittest/unittest.dart';
-export 'package:unittest/unittest.dart';
-
-describe(spec, fn) => group(spec, fn);
-it(spec, fn) => test(spec, fn);
